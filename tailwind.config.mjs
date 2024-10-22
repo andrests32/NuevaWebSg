36ab749d8946d9f3b0f5f247ae0909444acc5b6e
Andres Terraza <andresterraza.at@gmail.com>
2024-10-21 22:18:07 -0500

    Mejoramos la parte de la seccion de hero con un carrusel para las images de los proyectos tambien agregamos la informacin como tel, correo, dir debajo del navbas para que sea mas accesible la informacion.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		"./node_modules/flowbite/**/*.js"
-	],
-	theme: {
-		extend: {},
-		fontFamily: {
-			'monse': 'Montserrat, sans-serif',
-			'roboto': 'Roboto, sans-serif',
-			'roboto-mono': 'Roboto Mono, monospace',
-			'roboto-slab': 'Roboto Slab, serif',
-			'roboto-condensed': 'Roboto Condensed, sans-serif',
-			'roboto-flex': 'Roboto Flex, sans-serif',
-			'roboto-serif': 'Roboto Serif, serif',
-			'roboto-slab': 'Roboto Slab, serif',
-			'roboto-slab': 'Roboto Slab, serif',
-		}	
-	},
-	plugins: [
-		require('flowbite/plugin')
-	],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      monse: "Montserrat, sans-serif",
+      roboto: "Roboto, sans-serif",
+      "roboto-mono": "Roboto Mono, monospace",
+      "roboto-slab": "Roboto Slab, serif",
+      "roboto-condensed": "Roboto Condensed, sans-serif",
+      "roboto-flex": "Roboto Flex, sans-serif",
+      "roboto-serif": "Roboto Serif, serif",
+      "roboto-slab": "Roboto Slab, serif",
+      "roboto-slab": "Roboto Slab, serif"
+    },
+    // colors: {
+    //   primary: "#1B2534",
+    //   secondary: "#4b5563",
+    //   accent: "#000000"
+    // }
+  },
+  plugins: [require("flowbite/plugin")]
+};
